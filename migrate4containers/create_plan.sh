3f06d42e521d7823bb4c5c5d1cf8787f94162a0a
evekhm <evekhm@gmail.com>
2022-08-18 15:07:31 +0300

    fixed typo

--- migrate4containers/create_plan.sh
+++ migrate4containers/create_plan.sh
@@ -4,13 +4,14 @@ set -e # Exit if error is detected during pipeline execution
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #1. Create a processing cluster
+echo "Creating a processing cluster..."
 gcloud container clusters create $CLUSTER  --project=$PROJECT_ID \
   --zone="$ZONE" --machine-type e2-standard-4   --image-type ubuntu_containerd \
   --num-nodes "$NUM_NODES" --enable-stackdriver-kubernetes   \
   --subnetwork "projects/$PROJECT_ID/regions/$REGION/subnetworks/default"
 
 #2. Install Migrate to Containers
-
+echo "Creating service account for the migration..."
 #service account for the migration and initialize Migrate to Containers
 gcloud iam service-accounts create $SA_INSTALL \
   --project=$PROJECT_ID
@@ -29,14 +30,17 @@ gcloud iam service-accounts keys create "$SA_INSTALL".json \
 gcloud container clusters get-credentials $CLUSTER --zone $ZONE
 
 #Set up Migrate to Containers
+echo "Installing Migrate to Containers..."
 migctl setup install --json-key="$SA_INSTALL".json --gcp-project $PROJECT_ID --gcp-region $REGION
 
 #Validate the Migrate to Containers installation
+echo "Validating Migration..."
 migctl doctor
 
 #3 Migrating the VM
 
 #Create a service account for Compute Engine source migrations
+echo "Creating a service account for Compute Engine source migrations..."
 gcloud iam service-accounts create "$SA_MIGRATE" \
   --project=$PROJECT_ID
 
@@ -56,12 +60,15 @@ gcloud iam service-accounts keys create "$SA_MIGRATE".json \
   --project="$PROJECT_ID"
 
 #Create the migration source:
-migctl source create ce source-vm --project "$PROJECT_ID" --json-key="$SA_MIGRATE".json
+echo "Creating the migration source..."
+migctl source create ce "$SRC_VM" --project "$PROJECT_ID" --json-key="$SA_MIGRATE".json
 
 #Create a migration
+echo "Creating the migration job..."
 migctl migration create "$MIGRATION_JOB" --source source-vm   --vm-id "$SRC_VM" --type linux-system-container
 
 #check the status
+echo "Chechking the status of the migration job..."
 migctl migration status "$MIGRATION_JOB"
 
 #Review the migration plan
